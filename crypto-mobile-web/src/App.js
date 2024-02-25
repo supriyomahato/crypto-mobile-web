d045430590e016208f51554044fa9c9e8c54db6c
supriyomahato <64798279+supriyomahato@users.noreply.github.com>
2024-02-25 17:52:15 +0000

    create first part

--- crypto-mobile-web/src/App.js
+++ crypto-mobile-web/src/App.js
@@ -4,7 +4,13 @@ import TopHead from './components/TopHead';
 function App() {
   return (
     <div className="App">
-     < TopHead/>
+      <div className="top">< TopHead/></div>
+      <div className="top-1">
+        <h5>Day</h5>
+        <h5>Week</h5>
+        <h5>Month</h5>
+        <h5>Year</h5>
+      </div>
     </div>
   );
 }
